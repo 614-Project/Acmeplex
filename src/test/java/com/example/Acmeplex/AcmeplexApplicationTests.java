dc3d4bd3991e5c78eec826de82c0431d23e94dbb
Laxmi Paudel <meluxmeme@gmail.com>
2024-11-17 22:09:32 -0700

    maven based spring-boot project set up

---
+++ src/test/java/com/example/Acmeplex/AcmeplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Acmeplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
