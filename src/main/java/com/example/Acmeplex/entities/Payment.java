750ced18cbea1face500f5316c3ea33788cdcee2
Laxmi Paudel <meluxmeme@gmail.com>
2024-11-22 01:30:04 -0700

    Set up .env for payment

---
+++ src/main/java/com/example/Acmeplex/entities/Payment.java
@@ -0,0 +1,32 @@
+package com.example.Acmeplex.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "payments")
+@Data
+public class Payment {
+
+
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "amount")
+    private double amount;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
